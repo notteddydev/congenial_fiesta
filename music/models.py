e4b2fccf603f135c61d0e08cdce5b3a721956f57
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-17 20:18:17 +0100

    Bin pre_save signals and replace with save overrides. Bin Participation model. Move ManyToMany from Artist to Tune. Auto-set tune.file_name, and update it when Artist is updated.

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 
@@ -10,24 +10,10 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        ordering = ["name"]
-
-
-@receiver(pre_save, sender=Tag)
-def set_slug(sender: Tag, instance: Tag, **kwargs: dict):
-    instance.slug = slugify(instance.name)
-
-
-class Tune(models.Model):
-    name = models.CharField(max_length=150)
-    file_name = models.CharField(blank=True, editable=False, max_length=200, unique=True)
-    tags = models.ManyToManyField(Tag)
-    youtube_id = models.CharField(blank=True, max_length=75, unique=True)
-
-    def __str__(self):
-        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["name"]
@@ -35,7 +21,6 @@ class Tune(models.Model):
 
 class Artist(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    tunes = models.ManyToManyField(Tune, through="Participation")
 
     def __str__(self):
         return self.name
@@ -43,8 +28,41 @@ class Artist(models.Model):
     class Meta:
         ordering = ["name"]
 
+@receiver(post_save)
+def update_tune_file_names(sender: Artist, instance: Artist, **kwargs):
+    tunes = Tune.objects.filter(artists__id=instance.id)
 
-class Participation(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    tune = models.ForeignKey(Tune, on_delete=models.CASCADE)
-    remixed = models.BooleanField(default=False)
+    for tune in tunes:
+        tune.save()
+
+
+class Tune(models.Model):
+    name = models.CharField(max_length=150)
+    file_name = models.CharField(blank=True, editable=False, max_length=200, unique=True)
+    artists = models.ManyToManyField(Artist)
+    tags = models.ManyToManyField(Tag)
+    youtube_id = models.CharField(blank=True, max_length=75, unique=True)
+
+    def __str__(self):
+        strTune = ''
+
+        for artist in self.artists.all():
+            strTune += f"{artist.name}, "
+
+        return f"{strTune[:-2]} - {self.name}"
+    
+    def get_file_name(self):
+        file_name = ''
+        artists = Artist.objects.filter(tune=self.id)
+
+        for artist in artists.all():
+            file_name += f"{slugify(artist.name)}_and_"
+
+        return f"{file_name[:-5]}-_-{slugify(self.name)}"
+    
+    def save(self, *args, **kwargs):
+        self.file_name = self.get_file_name()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ["name"]
