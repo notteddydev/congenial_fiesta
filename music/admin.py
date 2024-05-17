7a7d94f140de43b4c374eea5131256a86735f61f
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-17 20:19:31 +0100

    Remove Participation import and registration.

--- music/admin.py
+++ music/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Artist, Participation, Tag, Tune
+from .models import Artist, Tag, Tune
 
 
 admin.site.register(Artist)
-admin.site.register(Participation)
 admin.site.register(Tag)
 admin.site.register(Tune)
