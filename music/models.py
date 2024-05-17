d6433a2b3ab5c5d38e1a84920fd63e6456155a33
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-17 11:27:00 +0100

    Basic model creation.

--- music/models.py
+++ music/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=75, unique=True)
+    slug = models.SlugField(db_index=True, editable=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+@receiver(pre_save, sender=Tag)
+def set_slug(sender: Tag, instance: Tag, **kwargs: dict):
+    instance.slug = slugify(instance.name)
+
+
+class Tune(models.Model):
+    name = models.CharField(max_length=150)
+    file_name = models.CharField(blank=True, editable=False, max_length=200, unique=True)
+    tags = models.ManyToManyField(Tag)
+    youtube_id = models.CharField(blank=True, max_length=75, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    tunes = models.ManyToManyField(Tune, through="Participation")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Participation(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    tune = models.ForeignKey(Tune, on_delete=models.CASCADE)
+    remixed = models.BooleanField(default=False)
