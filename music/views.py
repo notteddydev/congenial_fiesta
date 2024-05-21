62cae5c5023781ead205969544a129cc7811a602
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-21 09:48:30 +0100

    Update ListenView to use new download method.

--- music/views.py
+++ music/views.py
@@ -19,18 +19,7 @@ class TuneListenView(View):
     def post(self, request, id):
         tune = Tune.objects.get(pk=id)
 
-        try:
-            yt = YouTube(tune.youtube_link)
-        except VideoUnavailable:
-            return HttpResponseNotFound("YouTube video not found.")
-
-        video = yt.streams.filter(progressive=True).first()
-        mp3 = video.download(filename=tune.file_name, output_path=os.environ.get('TUNES_DIR'))
-
-        if mp3 is None:
-            return HttpResponseNotFound("YouTube video not found.")
+        if tune.download():
+            return HttpResponseRedirect(reverse("tune-list"))
         
-        song = AudioSegment.from_file(mp3)
-        song.export(tune.full_file_path, format="mp3")
-
-        return HttpResponseRedirect(reverse("tune-list"))
+        return HttpResponseNotFound("YouTube video not found.")
