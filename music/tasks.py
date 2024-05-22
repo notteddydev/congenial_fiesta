65246929e7692bae622b8d5a4437d40b61f22892
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-22 13:45:19 +0100

    Change some variable names for readability. Use new util method get_tune_id_from_file_name for consistency. Create new shared task tune_update_file_name to update the file name of a tune and move the corresponding file, when the tune.name is updated.

--- music/tasks.py
+++ music/tasks.py
@@ -5,6 +5,8 @@ from celery import shared_task
 from django.db import IntegrityError, transaction
 from django.db.models import F
 
+from .utils import get_tune_id_from_file_name
+
 
 @shared_task
 def tune_download(tune_id):
@@ -16,6 +18,33 @@ def tune_download(tune_id):
     tune.set_file_name()
     if tune.download():
         tune.save()
+
+
+@shared_task
+def tune_update_file_name(tune_id):
+    print(f"Running tune_update_file_name for tune with id: {tune_id}")
+
+    from .models import Tune
+
+    tune = Tune.objects.get(pk=tune_id)
+
+    tune_file_names = os.listdir(os.environ.get('TUNES_DIR'))
+
+    tune_gen = (
+        tune_file_name for tune_file_name in tune_file_names
+        if tune_id == get_tune_id_from_file_name(tune_file_name)
+    )
+    existing_file_name = next(tune_gen, None)
+
+    if existing_file_name is None:
+        tune.file_name = f"{tune.youtube_id}.mp3"
+        tune.save()
+        return
+     
+    tune.set_file_name().save()
+    current_file_path = f"{os.environ.get('TUNES_DIR')}/{existing_file_name}"
+    new_file_path = f"{os.environ.get('TUNES_DIR')}/{tune.file_name}"
+    move(current_file_path, new_file_path)
 
 
 @shared_task
@@ -36,13 +65,11 @@ def tune_update_file_names_for_artist(artist_id):
     except IntegrityError:
         raise IntegrityError(f"Tunes couldn't be updated for artist with id: {artist_id}")
 
-    tunes_list = os.listdir(os.environ.get('TUNES_DIR'))
+    tune_file_names = os.listdir(os.environ.get('TUNES_DIR'))
 
-    for tune_file_name in tunes_list:
+    for tune_file_name in tune_file_names:
         current_path = f"{os.environ.get('TUNES_DIR')}/{tune_file_name}"
-
-        file_name_split = tune_file_name.split('-_-')
-        tune_id = int(file_name_split[2][:-4])
+        tune_id = get_tune_id_from_file_name(tune_file_name)
         tune = tunes.get(pk=tune_id)
 
         new_path = f"{os.environ.get('TUNES_DIR')}/{tune.file_name}"
