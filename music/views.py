b68fa36dfac503e66f8a9592278a726c770dae22
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-22 14:21:53 +0100

    Update ListenView to work as the task does. ListenView maintained currently for in case task fails.

--- music/views.py
+++ music/views.py
@@ -2,13 +2,12 @@ import os
 
 from django.shortcuts import render
 from django.views.generic import ListView, View
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from pydub import AudioSegment
-from pytube import YouTube
 from pytube.exceptions import VideoUnavailable
 
 from .models import Tune
+from .tasks import tune_download
 
 
 class TuneListView(ListView):
@@ -18,8 +17,10 @@ class TuneListView(ListView):
 class TuneListenView(View):
     def post(self, request, id):
         tune = Tune.objects.get(pk=id)
-
+        tune.set_file_name()
+        
         if tune.download():
+            tune.save()
             return HttpResponseRedirect(reverse("tune-list"))
         
         return HttpResponseNotFound("YouTube video not found.")
