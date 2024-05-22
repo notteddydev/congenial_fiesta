7923972ec798bfbe7b91e102b2e9aa25645d1f37
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-22 18:45:51 +0100

    Change appearance of tunes list page in admin for easier readability.

--- music/admin.py
+++ music/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import Artist, Tag, Tune
 
 
+class TuneAdmin(admin.ModelAdmin):
+
+    def view_artists(self, obj):
+        return ", ".join([artist.name for artist in obj.artists.all()])
+    
+    list_display = ("name", "view_artists",)
+
+
 admin.site.register(Artist)
 admin.site.register(Tag)
-admin.site.register(Tune)
+admin.site.register(Tune, TuneAdmin)
