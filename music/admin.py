5a926b5e382025e485cdc3774cb0df2860f55622
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-23 07:26:32 +0100

    Add admin action to download multiple. Update typehints from Any to Tune.

--- music/admin.py
+++ music/admin.py
@@ -1,6 +1,5 @@
 import os
 
-from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
@@ -10,13 +9,22 @@ from .models import Artist, Tag, Tune
 
 class TuneAdmin(admin.ModelAdmin):
     @admin.action(description="Set the title and artist metadata")
-    def set_queryset_metadata(self, request: HttpRequest, queryset: QuerySet[Any]):
+    def set_queryset_metadata(self, request: HttpRequest, queryset: QuerySet[Tune]):
         for tune in queryset:
             if tune.downloaded:
                 tune.set_metadata()
 
 
-    def delete_queryset(self, request: HttpRequest, queryset: QuerySet[Any]) -> None:
+    @admin.action(description="Download")
+    def download_queryset(self, request: HttpRequest, queryset: QuerySet[Tune]):
+        for tune in queryset:
+            if not tune.downloaded:
+                tune.set_file_name()
+                if tune.download():
+                    tune.save()
+
+
+    def delete_queryset(self, request: HttpRequest, queryset: QuerySet[Tune]) -> None:
         for tune in queryset:
             tune.remove_file()
 
@@ -25,7 +33,7 @@ class TuneAdmin(admin.ModelAdmin):
     def view_artists(self, obj):
         return ", ".join([artist.name for artist in obj.artists.all()])
     
-    actions = [set_queryset_metadata]
+    actions = [download_queryset, set_queryset_metadata]
     list_display = ("name", "view_artists",)
 
 
