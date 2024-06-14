a4e0884d97b47de685311a48428f2194635b783c
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-14 14:17:48 +0100

    New view created for getting info on a RawTuneString and redirecting to the delete page.

--- music/views.py
+++ music/views.py
@@ -1,13 +1,14 @@
-import os
+import re
+import subprocess
+import urllib.parse
+import urllib.request
 
-from django.shortcuts import render
-from django.views.generic import ListView, View
 from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from pytube.exceptions import VideoUnavailable
+from django.views.generic import ListView, View
 
-from .models import Tune
-from .tasks import tune_download
+from .models import RawTuneString, Tune
 
 
 class TuneListView(ListView):
@@ -24,3 +25,40 @@ class TuneListenView(View):
             return HttpResponseRedirect(reverse("tune-list"))
         
         return HttpResponseNotFound("YouTube video not found.")
+    
+class RawTuneStringInfoView(View):
+    def get(self, request, id):
+        brave_path = '/usr/bin/brave-browser'
+        rts = get_object_or_404(RawTuneString, id=id)
+
+        search_keyword = urllib.parse.quote(rts.info, safe='')
+        yt_search_url = f"https://www.youtube.com/results?search_query={search_keyword}"
+        html = urllib.request.urlopen(yt_search_url)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+
+        distinct_video_ids = list(dict.fromkeys(video_ids))
+        first_video_ids = distinct_video_ids[:5]
+
+        genius_query = urllib.parse.quote(f"site:genius.com {rts.info}")
+        google_query = urllib.parse.quote(rts.info)
+        genre_query = urllib.parse.quote(f"genre {rts.info}")
+
+        urls = [
+            f"https://www.youtube.com/watch?v={first_video_ids[0]}",
+            yt_search_url,
+            f"https://www.google.com/search?q={genius_query}",
+            f"https://www.google.com/search?q={google_query}",
+            f"https://www.google.com/search?q={genre_query}"
+        ]
+
+        duplicates = Tune.objects.filter(youtube_id__in=first_video_ids).all()
+
+        if len(duplicates):
+            for dupe in duplicates:
+                urls.append(request.build_absolute_uri(reverse("admin:music_tune_change", args=[dupe.id])))
+
+        for url in urls:
+            subprocess.run([brave_path, '--incognito', '--new-tab', url])
+
+        # return HttpResponseRedirect(request.META["HTTP_REFERER"])
+        return HttpResponseRedirect(reverse("admin:music_rawtunestring_delete", args=[rts.id]))
