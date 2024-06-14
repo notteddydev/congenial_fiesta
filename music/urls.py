07efb7db122c316ffaf1ca8e443effa842357227
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-14 14:18:16 +0100

    Register new view in urls.py.

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("tunes", views.TuneListView.as_view(), name="tune-list"),
-    path("tunes/<int:id>/listen", views.TuneListenView.as_view(), name="tune-listen")
+    path("tunes/<int:id>/listen", views.TuneListenView.as_view(), name="tune-listen"),
+    path("raw-tune-strings/<int:id>/info", views.RawTuneStringInfoView.as_view(), name="rawtunestring-info")
 ]
